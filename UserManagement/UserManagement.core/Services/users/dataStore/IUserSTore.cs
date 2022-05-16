d47be28f406fe57cc3311304d10c9d938645400d
Alfred Obialo <alfredcsdinc@gmail.com>
2022-05-16 05:42:23 +0100

    User Manager: Implement Update User Profile

--- UserManagement/UserManagement.core/Services/users/dataStore/IUserSTore.cs
+++ UserManagement/UserManagement.core/Services/users/dataStore/IUserSTore.cs
@@ -1,4 +1,5 @@
 ﻿using asom.lib.core;
+using UserManagement.core.Services.users.reqRes;
 using UserManagement.core.shared;
 using CommandResponse = asom.lib.core.CommandResponse;
 
@@ -7,6 +8,7 @@ namespace UserManagement.core.Services.users.dataStore;
 public interface IUserStore
 {
     Task<CommandResponse> CreateUser(AppUserEntity user);
+    Task<CommandResponse> UpdateUserName(UserProfileNameUpdateRequest request);
     Task<CommandResponse<AppUserEntity>> GetUser(string userId);
     Task<PagedCommandResponse<IEnumerable<AppUserEntity>>> GetUser(PagedDataCriteria criteria);
 }
