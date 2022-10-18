586a42ebe2ee162bf15fe84dd157efeaf7357f35
Alfred Obialo <alfredcsdinc@gmail.com>
2022-10-18 06:21:25 +0100

    Add Controller Decoration for User Profile

--- UserManager.api/Controllers/UserProfilePictureController.cs
+++ UserManager.api/Controllers/UserProfilePictureController.cs
@@ -1,7 +1,10 @@
 ﻿using MediatR;
+using Microsoft.AspNetCore.Mvc;
 
 namespace UserManager.api.Controllers;
 
+[ApiController]
+[Route("profile-picture")]
 public class UserProfilePictureController :BaseController
 {
     public UserProfilePictureController(IMediator mediator) : base(mediator)
