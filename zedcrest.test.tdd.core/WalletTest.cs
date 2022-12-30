205249b2dc51e45bbba8f4bd1744281631232a2b
Alfred Obialo <alfredcsdinc@gmail.com>
2022-12-30 11:40:42 +0100

    First Passing Test

--- zedcrest.test.tdd.core/WalletTest.cs
+++ zedcrest.test.tdd.core/WalletTest.cs
@@ -1,3 +1,4 @@
+using FluentAssertions;
 using zedcrest.wallet.core.models;
 
 namespace zedcrest.test.tdd.core;
@@ -14,58 +15,14 @@ public class WalletTest
             .AddUsdWallet()
             .AddNgnWallet()   // we can't have the same wallet entity
            ;
-        WalletAccount walletAccount = builder.Build();
+      
         // act : on it
-
-        
+          IWalletAccount walletAccount = builder.Build();
         
         
         // assert
+        walletAccount.Should().NotBeNull();
+
     }
 }
 
-public class WalletAccount  : IWalletAccount
-{
-    private Dictionary<string, WalletAccount> _walletAccounts = new Dictionary<string, WalletAccount>();
-    public WalletUser Owner { get; internal set; }
-    public DateTimeOffset DateCreated { get; set; }
-
-    public void Add(Wallet walletCurrency)
-    {
-    }
-}
-
-public interface IWalletAccount
-{
-    void Add(Wallet walletCurrency);
-
-}
-
-public class Wallet
-{
-    public string Currency { get; set; }
-    public decimal Balance { get; set; }
-}
-
-public class WalletBuilder
-{
-    public WalletAccount Build()
-    {
-        return null;
-    }
-
-    public WalletBuilder CreateWallet(WalletUser @new)
-    {
-        return null;
-    }
-
-    public WalletBuilder AddUsdWallet()
-    {
-        return null;
-    }
-
-    public WalletBuilder AddNgnWallet()
-    {
-        return null;
-    }
-}
